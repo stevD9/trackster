956f8360995367c2b5698be1138004709c2e5f37
Fredericia <39918560+stevD9@users.noreply.github.com>
2018-10-01 19:01:01 +0300

    Created API request to search by song title

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 });
 
 var Trackster = {};
-
+const API_KEY = '4704092f4b581c3c68a01526db6a7ba6';
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
@@ -19,5 +19,11 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track='+title+'&api_key='+API_KEY+'&format=json',
+    datatype: 'jsonp',
+    success: function(data) {
+      console.log(data);
+    }
+    });
 };
